mvaisakh/android_device_asus_X01BD

X01BD: Use 4GB dalvik-heap config
We are currently using 2GB dalvik-heap config.

Bug: 145823510
Test: MPTS test
Change-Id: Ie26b8e4fefea25fa818bd1eaf9df4a0bb153d42d
Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

device.mk
@@ -20,6 +20,9 @@ $(call inherit-product, vendor/asus/X01BD/X01BD-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # HIDL HALs
 $(call inherit-product, $(LOCAL_PATH)/hidl-hals.mk)
 

FarhanPatel88/android_device_asus_X01BD-1

device.mk
@@ -20,6 +20,9 @@ $(call inherit-product, vendor/asus/X01BD/X01BD-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, vendor/pixeldust/configs/phone-xhdpi-4096-dalvik-heap.mk)
+
 # HIDL HALs
 $(call inherit-product, $(LOCAL_PATH)/hidl-hals.mk)
 